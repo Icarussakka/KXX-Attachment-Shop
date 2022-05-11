ad86d285753e753907a017fc36b671b7a8fb0cf2
Kreannix <1dufotze@gmail.com>
2022-05-11 15:27:26 +0200

    created addon structure

--- lua/autorun/att_shop-init.lua
+++ lua/autorun/att_shop-init.lua
@@ -1,1 +1,16 @@
---
+NMG = NMG or {}
+NMG.AttachmentShop = NMG.AttachmentShop or {}
+
+if SERVER then
+    util.AddNetworkString("NGM.AttachmentShop.BuyAttachments")
+
+    if (VoidLib) then
+        include("attachment_shop/sv_database.lua")
+        print("[NMG] Attachment Shop Loaded")
+    else
+        hook.Add("VoidLib.Loaded", "NMG.AttachmentShop.Init.WaitForVoidLib", function ()
+            include("attachment_shop/sv_database.lua")
+            print("[NMG] Attachment Shop Loaded")
+        end)
+    end
+end
