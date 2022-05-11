4546661de0b2c4c89e2176375cb5f18a71a57c3f
Kreannix <1dufotze@gmail.com>
2022-05-11 15:44:56 +0200

    created base vgui structure

---
+++ lua/attachment_shop/vgui/attachment_shop-panel.lua
@@ -0,0 +1,19 @@
+local PANEL = {}
+
+function PANEL:Init()
+    
+end
+
+function PANEL:GetPresets()
+
+end
+
+function PANEL:GetAttachments()
+    
+end
+
+function PANEL:GetShoppingCart()
+
+end
+
+vgui.Register("NMG.AttachmentShop.Panel", PANEL, "VoidUI.Frame")
