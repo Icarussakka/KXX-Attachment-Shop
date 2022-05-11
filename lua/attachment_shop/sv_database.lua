6ba3eb5d7c02b4636a2112f7cf0b71faa597ba21
Kreannix <1dufotze@gmail.com>
2022-05-11 15:38:48 +0200

    added database

---
+++ lua/attachment_shop/sv_database.lua
@@ -0,0 +1,45 @@
+local database = VoidLib.Database:Create({
+    host = "localhost",
+    username = "username",
+    password = "password",
+    database = "database",
+    port = 3306,
+    useMySQL = false,
+})
+
+function database:OnConnected()
+    local query = database:Create("ngm_attachmenshop")
+        query:Create("id", "INTEGER NOT NULL AUTO_INCREMENT")
+        query:Create("steam_id", "VARCHAR(35) NOT NULL")
+        query:Create("preset_id", "INTEGER")
+        query:Create("preset_item", "VARCHAR(35) NOT NULL")
+        query:PrimaryKey("id")
+    query:Execute()
+end
+
+function NMG.AttachmentShop.InsertPresetItem(steam_id, preset_id, preset_item)
+    local query = database:Insert("ngm_attachmenshop")
+        query:Insert("steam_id", steam_id)
+        query:Insert("preset_id", preset_id)
+        query:Insert("preset_item", preset_item)
+    query:Execute()
+end
+
+function NMG.AttachmentShop.DeletePreset(steam_id, preset_id)
+    local query = database:Delete("ngm_attachmenshop")
+        query:Where("steam_id", steam_id)
+        query:Where("preset_id", preset_id)
+    query:Execute()
+end
+
+function NMG.AttachmentShop.SelectPresetItem(steam_id, preset_id, callback)
+    local query = database:Delete("ngm_attachmenshop")
+        query:Where("steam_id", steam_id)
+        query:Where("preset_id", preset_id)
+        query:Callback(function (tblData)
+            for k, preset in ipairs(tblData) do
+                // coming
+            end
+        end)
+    query:Execute()
+end
