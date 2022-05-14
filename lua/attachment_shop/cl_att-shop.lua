b24874918886d02f768a16867a25c4310a1d64ca
Kreannix <1dufotze@gmail.com>
2022-05-15 01:02:11 +0200

    removed debugging tools

--- lua/attachment_shop/cl_att-shop.lua
+++ lua/attachment_shop/cl_att-shop.lua
@@ -1,8 +1,6 @@
 net.Receive("NMG.AttachmentShop.SendPresetToClient", function(len)
     local indexNumber = net.ReadUInt(5)
 
-    print("angekommen")
-
     for i = 1, indexNumber do
         local attachment = net.ReadString()
 
